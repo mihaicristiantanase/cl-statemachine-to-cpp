fa794d47aa5d88951f54e5e3e8d26b912f9c48c4
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2024-05-01 20:18:02 +0300

    Improve genrated usage code

--- cl-statemachine-to-c++.lisp
+++ cl-statemachine-to-c++.lisp
@@ -364,9 +364,12 @@
             (wl)
             (wl "std::cout << \"This returns a specific exception:\" << std::endl;")
             (define-c++-block "sm.doActionExecuteSomething([&](bool success, std::exception& e)"
-                (define-c++-block "if (!success)"
-                  (wl "  std::cout << \"-- success: false\" << std::endl;")
-                  (wl "  std::cout << \"-- error: \" << sm.errorDescription() << std::endl;")))
+              (wl "std::cout << \"-- success:\" << success << std::endl;")
+              (wl "std::cout << \"-- error: \" << sm.errorDescription() << std::endl;"))
+            (wl ");")
+            (wl "std::cout << \"This moves through various states:\" << std::endl;")
+            (define-c++-block "sm.doActionGoToB([&](bool success, std::exception& e)"
+              (wl "std::cout << \"-- success:\" << success << std::endl;"))
             (wl ");")))
     (define-c++-class-section "private"
         (dolist (action (slot-value *machine* 'actions))
