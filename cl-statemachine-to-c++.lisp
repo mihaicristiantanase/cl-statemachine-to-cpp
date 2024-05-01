9922eee23bb549b7ee7f1ac4217d947b60865714
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2024-05-01 13:36:23 +0300

    Run the generated executable

--- cl-statemachine-to-c++.lisp
+++ cl-statemachine-to-c++.lisp
@@ -398,7 +398,8 @@
     (with-open-file (f path-usage :direction :output :if-exists :supersede)
       (write-string code-usage f))
     (format t "~&Verifying output by compiling…~%")
-    (format t "~&~a~%" (shell:run t "g++" "-std=c++11" "-g" "-o" "/tmp/sm" path-usage))
+    (format t "~&~{~a~%~}~%" (shell:run t "g++" "-std=c++11" "-g" "-o" "/tmp/sm" path-usage))
+    (format t "~&~{~a~%~}~%" (shell:run t "/tmp/sm"))
     (format t "~&Done. Please check ~a for the generated C++ file~% ~
                  and ~a for a sample code of how to use."
             path-code path-usage)))
