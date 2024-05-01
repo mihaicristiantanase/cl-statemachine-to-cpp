b2fde91fe4242b32e5f811265fc5247f0bbeea63
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2024-05-01 13:29:58 +0300

    Improve output verification with output binary destination and debug symbols

--- cl-statemachine-to-c++.lisp
+++ cl-statemachine-to-c++.lisp
@@ -392,7 +392,7 @@
     (with-open-file (f path-usage :direction :output :if-exists :supersede)
       (write-string code-usage f))
     (format t "~&Verifying output by compiling…~%")
-    (format t "~&~a~%" (shell:run t "g++" "-std=c++11" path-usage))
+    (format t "~&~a~%" (shell:run t "g++" "-std=c++11" "-g" "-o" "/tmp/sm" path-usage))
     (format t "~&Done. Please check ~a for the generated C++ file~% ~
                  and ~a for a sample code of how to use."
             path-code path-usage)))
